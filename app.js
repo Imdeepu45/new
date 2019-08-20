c7e915a8ac5b24ca6bcf573fd940366509d2160c
Dilee Deepu <dileepdeepu1995@yahoo.com>
2019-08-20 13:37:15 +0530

    clg

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const Express=require("express");
     var  bodyparser=require('body-parser');
     const mongoose=require('mongoose');
+    const Request=require('request');
+
 
     var app=new Express();
 
+
     app.set('view engine','ejs');
+
+
 
    app.use(Express.static(__dirname+"/public"));
    app.use(bodyparser.json());
     app.use(bodyparser.urlencoded({extended:true}));
+
+
 
     const studentModel=mongoose.model("studentdetails",
     
@@ -23,20 +30,18 @@ const Express=require("express");
     mongoose.connect("mongodb://localhost:27017/collegedb");
 
 
-
-
-   
     app.get('/',(req,res)=> {
-        res.render('login');
-        console.log(name)
+        res.render("login");
+        
     });
     
-    app.post('/login',(req,res)=> {
+    app.post('/',(req,res)=> {
+
 
             console.log(req,body);
 
             var student=new studentModel(req,body);
-            var result=student.save();
+            var result=student.save(error,data);
             res.send(result);
 
 
@@ -51,8 +56,37 @@ const Express=require("express");
        //console.log("College:"+college);
     });
 
+    app.get('/getdatas',(req,res)=>{
 
+
+        result=studentModel.find((error,data)=>{
+            if(error){
+                throw error;
+            }
+                else{
+                    res.send(data);
+                }
+        });
+    });
+
+    const getdataApi="http://localhost:3000/getdatas";
+
+
+    app.get('/view',(req,res)=>{
+        Request(getdataApi,(error,Response,body)=>{
+            var data=JSON.parse(body);
+
+
+
+            console.log(data);
+            res.render('view',{'data':data});
+        });
+        
+    });
     
+
+
+
     app.listen(process.env.PORT || 3000,()=>{
         console.log("Server running on port::3000...");
     });
